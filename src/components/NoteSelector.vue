e759fe3cbdbe773f07ffb49649292841fd401f46
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-08-28 09:08:40 -0400

    Adds changes

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: ["note"],
+  data: function () {
+    return {
+      currentNote: {},
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="note-selector">
+    <p class="note-selector-title">{{ note.body }}</p>
+    <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+  </div>
+</template>
